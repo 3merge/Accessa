a909c72564c8233d3a07a00846b4161a66813a9a
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-11 12:04:00 -0400

    chore: wip Estae footer

--- src/Footers/Estate/Estate.jsx
+++ src/Footers/Estate/Estate.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 import PhysicalProperties from '../PhysicalProperties';
+
+const properties = [
+  {
+    subtitle: 'LOCATION',
+    body1: '405 Lake Road',
+    body2: 'Bowmanville ON L1C 4P8',
+  },
+  {
+    subtitle: 'CONTACT',
+    body1: 'crg@canadarubbergroup.com',
+    body2: '1.88.668.0646',
+  },
+];
+
+const logo = {
+  fluid: {
+    src: 'https://source.unsplash.com/random',
+  },
+  alt: 'placeholder',
+  style: {
+    width: '150px',
+    height: '150px',
+  },
+};
 
 const Estate = () => {
   return (
-    <footer>
-      <PhysicalProperties />
-    </footer>
+    <Box component="footer">
+      <PhysicalProperties
+        logo={logo}
+        properties={properties}
+      />
+    </Box>
   );
 };
 
--- src/Footers/PhysicalProperties/PhysicalProperties.jsx
+++ src/Footers/PhysicalProperties/PhysicalProperties.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import Image from 'gatsby-image';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  subtitle1: { fontWeight: 'bold', marginBottom: '15px' },
+});
+
+const Property = ({ subtitle, body1, body2 }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <Typography
+        className={classes.subtitle1}
+        variant="subtitle1"
+        gutterBottom
+      >
+        {subtitle}
+      </Typography>
+      <Typography variant="body1">{body1}</Typography>
+      {body2 ? (
+        <Typography variant="body1">{body2}</Typography>
+      ) : null}
+    </>
+  );
+};
 
 const PhysicalProperties = ({ logo, properties }) => {
   return (
-    <div>
-      <Grid container>
-        <p>hi</p>
-        <p>test</p>
-        <p>bye</p>
+    <Box>
+      {logo && (
+        <Box borderBottom={1} pb={5} mb={5}>
+          <Image
+            {...logo}
+            objectFit="cover"
+            objectPosition="50% 50%"
+          />
+        </Box>
+      )}
+      <Grid container spacing={10}>
+        {properties.map((prop) => (
+          <Grid item key={prop.subtitle}>
+            <Property {...prop} />
+          </Grid>
+        ))}
       </Grid>
-    </div>
+    </Box>
   );
 };
 export default PhysicalProperties;
