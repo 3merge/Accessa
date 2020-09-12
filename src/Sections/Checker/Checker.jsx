a18a6cc27c6c9e5050c55a74e7fa2e0ee1e7531e
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-11 21:02:13 -0400

    chore: setup checker

---
+++ src/Sections/Checker/Checker.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
+const Checker = () => {
+  return null;
+};
+
+export default Checker;
--- src/Sections/index.js
+++ src/Sections/index.js
@@ -2,3 +2,4 @@ export { default as Gale } from './Gale';
 export { default as Vitos } from './Vitos';
 export { default as Sonus } from './Sonus';
 export { default as News } from './News';
+export { default as Checker } from './Checker';
