cfb404fc3b5c757cc24532678655833735e7be1c
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-14 09:31:44 -0400

    chore: wip checker

--- src/Sections/Checker/Checker.jsx
+++ src/Sections/Checker/Checker.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Checker = () => {
-  return null;
+const useStyles = makeStyles(() => ({
+  root: ({ backgroundColor }) => ({
+    backgroundColor,
+  }),
+  title: {
+    fontWeight: 'bold',
+  },
+  main: {
+    fontWeight: 'bold',
+  },
+}));
+
+const Checker = ({ title, main, backgroundColor }) => {
+  const classes = useStyles({ backgroundColor });
+  return (
+    <Box
+      className={classes.root}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Typography
+        className={classes.title}
+        variant="subtitle1"
+      >
+        {title}
+      </Typography>
+      <Typography className={classes.main} variant="h5">
+        {main}
+      </Typography>
+    </Box>
+  );
 };
 
 export default Checker;
--- src/Sections/Sections.stories.jsx
+++ src/Sections/Sections.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withA11y } from '@storybook/addon-a11y';
 import SonusSection from './Sonus';
 import VitosSection from './Vitos';
+import Checker from './Checker';
 import { articles } from '../../cypress/fixtures/sections';
 
 export default {
@@ -14,3 +15,14 @@ export const Sonus = () => (
 );
 
 export const Vitos = () => <VitosSection />;
+
+export const content = {
+  title: 'TRAVEL MUCH?',
+  main:
+    'We have traveled to an average of 5 countries each',
+  backgroundColor: 'yellow',
+};
+
+export const CheckersSample = () => (
+  <Checker {...content} />
+);
