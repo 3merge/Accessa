8c12b169a6ef5e36ab15a62ab34ed660d1d5b222
Mike <mibberson@3merge.ca>
2020-09-18 14:02:33 -0400

    fix: linting issues

--- src/Lists/Element/useStyles.js
+++ src/Lists/Element/useStyles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(({ typography, palette }) => ({
+const useStyles = makeStyles(({ typography }) => ({
   first: {
     gridColumn: '1 / -1',
     marginBottom: '1rem',
