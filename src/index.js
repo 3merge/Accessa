928b299b5ddceeb967f251e74b8fcaa1ec390d41
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-23 11:45:42 -0400

    export Hoc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * as Features from './Features';
 export * as Footers from './Footers';
 export * as Galleries from './Galleries';
 export * as Headers from './Headers';
+export * as Hocs from './Hocs';
 export * as Layouts from './Layouts';
 export * as Partials from './Partials';
 export * as Sections from './Sections';
