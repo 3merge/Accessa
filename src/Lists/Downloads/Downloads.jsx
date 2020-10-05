ab94b5514441a370ec7eeadf02512f877ac06b16
Takuya Hirata <takuyahirata4@gmail.com>
2020-10-05 15:59:40 -0400

    chore: setup Downloads component

---
+++ src/Lists/Downloads/Downloads.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { renderListSafely } from '../../Hocs';
+
+const Downloads = ({ lists }) => {
+  return <div>here</div>;
+};
+
+Downloads.propTypes = {};
+
+export default renderListSafely(Downloads);
