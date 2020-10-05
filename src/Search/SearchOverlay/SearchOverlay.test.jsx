d55325b5b0fcd19cc2ebf79adfd5154c159deb7b
Mike <mibberson@3merge.ca>
2020-10-05 15:49:00 -0400

    test(wip): todos

---
+++ src/Search/SearchOverlay/SearchOverlay.test.jsx
@@ -0,0 +1,9 @@
+describe('SearchOverlay', () => {
+  describe('"renderSearchResult"', () => {
+    it.todo(
+      'should return null when paramter does not contain keys',
+    );
+
+    it.todo('should filter object of keys without values');
+  });
+});
