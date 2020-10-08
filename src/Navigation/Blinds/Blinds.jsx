1d88897fb0cb4d8cc756224233a083eaae553d49
Takuya Hirata <takuyahirata4@gmail.com>
2020-10-08 06:51:08 -0400

    style: full width dropdown

--- src/Navigation/Blinds/Blinds.jsx
+++ src/Navigation/Blinds/Blinds.jsx
@@ -5,25 +5,18 @@ import useStyles from './useStyles';
 
 const Blinds = ({ items }) => {
   const { ul } = useStyles();
-  const [currentMenu, setCurrentMenu] = React.useState(-1);
-
-  const setMenu = (index) => () => setCurrentMenu(index);
-
-  const reset = () => setCurrentMenu(-1);
 
   return (
     <div style={{ backgroundColor: 'tomato' }}>
       <nav>
-        <Grid container component="ul" className={ul}>
-          {items.map((x, i) => (
-            <Dropdown
-              key={i}
-              item={x}
-              setMenu={setMenu(i)}
-              index={i}
-              reset={reset}
-              currentMenu={currentMenu}
-            />
+        <Grid
+          container
+          component="ul"
+          className={ul}
+          spacing={5}
+        >
+          {items.map((x) => (
+            <Dropdown key={x.main} item={x} />
           ))}
         </Grid>
       </nav>
--- src/Navigation/Blinds/Dropdown.jsx
+++ src/Navigation/Blinds/Dropdown.jsx
@@ -3,46 +3,62 @@ import { Grid, Collapse } from '@material-ui/core';
 import { Link } from '@reach/router';
 import useStyles from './useStyles';
 
-const Dropdown = ({
-  item,
-  setMenu,
-  reset,
-  currentMenu,
-  index,
-}) => {
+const Dropdown = ({ item }) => {
+  const [active, setActive] = React.useState(false);
+  const activate = () => setActive(true);
+  const deactivate = () => setActive(false);
+  const ref = React.useRef(null);
+
   const { ul } = useStyles();
+
+  const { top = 0, left = 0, height = 0 } =
+    ref?.current?.getBoundingClientRect() || {};
 
   return (
     <Grid item component="li">
       <Grid
         container
-        onMouseOver={setMenu}
-        onMouseLeave={reset}
-        onFocus={setMenu}
-        onBlur={reset}
+        onMouseOver={activate}
+        onMouseLeave={deactivate}
+        onFocus={activate}
+        onBlur={deactivate}
       >
-        <Grid item xs={12} tabIndex={0}>
+        <Grid item xs={12} tabIndex={0} ref={ref}>
           {item.main}
         </Grid>
         {item.subitems ? (
-          <Collapse in={currentMenu === index}>
-            <ul className={ul}>
-              {item.subitems.map((sub, i) => (
-                <Grid
-                  item
-                  xs={12}
-                  component="li"
-                  onBlur={
-                    i === item.subitems.length - 1
-                      ? reset
-                      : null
-                  }
-                >
-                  <Link to={sub.path}>{sub.sub}</Link>
-                </Grid>
-              ))}
-            </ul>
-          </Collapse>
+          <div>
+            <Collapse
+              in={active}
+              style={{
+                position: 'fixed',
+                top: top + height,
+                left: left * -1,
+                backgroundColor: 'tomato',
+                right: 0,
+                paddingLeft: left * 2,
+                paddingTop: height / 2,
+                paddingBottom: height,
+              }}
+            >
+              <ul className={ul}>
+                {item.subitems.map((sub, i) => (
+                  <Grid
+                    item
+                    xs={12}
+                    component="li"
+                    onBlur={
+                      i === item.subitems.length - 1
+                        ? deactivate
+                        : null
+                    }
+                  >
+                    <Link to={sub.path}>{sub.sub}</Link>
+                  </Grid>
+                ))}
+              </ul>
+            </Collapse>
+          </div>
         ) : null}
       </Grid>
     </Grid>
