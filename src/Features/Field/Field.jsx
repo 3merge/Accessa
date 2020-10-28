f92bb1a63656aa4290411a24d55d1d9a9e699573
unknown <ibberson1992@gmail.com>
2020-10-28 19:06:45 -0400

    fix: tab index on non-link fields

--- src/Features/Field/Field.jsx
+++ src/Features/Field/Field.jsx
@@ -28,48 +28,61 @@ const Field = ({ lists, fill, spacing, ...rest }) => {
       component="ul"
       className={ul}
     >
-      {lists.map((img, i) => (
-        <Grid
-          item
-          component="li"
-          key={i}
-          className={li}
-          {...rest}
-        >
-          <Card
-            onClick={img.onClick}
-            onKeyPress={img.onClick}
-            className={
-              img.onClick ? card : [card, noCta].join(' ')
-            }
-            tabIndex={0}
+      {lists.map((img, i) => {
+        const getCardProps = () =>
+          img.onClick
+            ? {
+                role: 'link',
+                className: card,
+                tabIndex: 0,
+              }
+            : {
+                className: [card, noCta].join(' '),
+              };
+
+        return (
+          <Grid
+            item
+            className={li}
+            component="li"
+            key={i}
+            {...rest}
           >
-            <Image
-              fluid={img.fluid}
-              imgStyle={{
-                objectFit: 'cover',
-              }}
-              style={{ width: '100%', height: '200px' }}
-            />
-            <CardContent>
-              {img.subtitle ? (
-                <span className={subtitle}>
-                  {img.subtitle}
-                </span>
-              ) : null}
-              <Typography
-                variant="h3"
-                className={cardTitle}
-              >
-                {img.title}
-              </Typography>
-              <Typography variant="body1" className={body1}>
-                {img.description}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
+            <Card
+              onClick={img.onClick}
+              onKeyPress={img.onClick}
+              {...getCardProps()}
+            >
+              <Image
+                fluid={img.fluid}
+                imgStyle={{
+                  objectFit: 'cover',
+                }}
+                style={{ width: '100%', height: '200px' }}
+              />
+              <CardContent>
+                {img.subtitle ? (
+                  <span className={subtitle}>
+                    {img.subtitle}
+                  </span>
+                ) : null}
+                <Typography
+                  variant="h3"
+                  className={cardTitle}
+                >
+                  {img.title}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  className={body1}
+                >
+                  {img.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
